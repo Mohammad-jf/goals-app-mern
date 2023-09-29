2088ce54c8587e2eb2310c3567d13db08f89ac7c
mohammad jalifathi <m.h.jalifathi@gmail.com>
2023-09-29 06:11:31 +0330

    errormiddleware

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -1,17 +1,37 @@
-const getGoals = (req, res) => {
+const asyncHandler = require('express-async-handler');
+
+// @desc get goals
+// @route GET /api/goals
+// @access privete
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'get Goals' });
-};
+});
 
-const createGoal = (req, res) => {
-  res.status(201).json({ message: 'goal created' });
-};
+// @desc create new goal
+// @route POST /api/goals
+// @access privete
+const createGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    // bad request
+    res.status(400);
+    throw new Error('please add a text');
+  }
 
-const updateGoal = (req, res) => {
+  res.status(201).json({ message: req.body.text });
+});
+
+// @desc update specific goal
+// @route PUT /api/goals/:id
+// @access privete
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `goal ${req.params.id} updated` });
-};
+});
 
-const deleteGoal = (req, res) => {
-  res.status(200).json({ message: `goal ${req.params.id} deleted` });
-};
+// @desc delete specific goal
+// @route DELETE /api/goals/:id
+// @access privete
+const deleteGoal = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `goal ${req.params.id} updated` });
+});
 
 module.exports = { getGoals, createGoal, updateGoal, deleteGoal };
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+
 // it allow us to have .env file
 const dotenv = require('dotenv').config();
+
+const { errorHandler } = require('./middlewares/errorMiddleware');
+
 const port = process.env.PORT || 5000;
 
 // init express app
 const app = express();
 
+// for accessing data in req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
 app.use('/api/goals', require('./routes/goalsRoutes'));
+
+//should be in bottom after everything to overWrite default express error handler)
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
