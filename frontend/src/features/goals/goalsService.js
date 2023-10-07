94d2b7327ad7d61b50c6f44b47eb270f516ca6eb
mohammad jalifathi <m.h.jalifathi@gmail.com>
2023-10-07 04:52:23 +0330

    goalSlice and goal service

--- frontend/src/features/goals/goalsService.js
+++ frontend/src/features/goals/goalsService.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 const API_URL = '/api/goals';
 
-const getGoals = async (userData) => {
+const getGoals = async (token) => {
   const response = await axios.get(API_URL);
-
   return response.data;
 };
 
-const createGoal = async (goalText) => {
+const createGoal = async (goalText,token) => {
   const response = await axios.post(API_URL, goalText);
   return response.data;
 };
--- frontend/src/features/goals/goalsSlice.js
+++ frontend/src/features/goals/goalsSlice.js
@@ -1,12 +1,83 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import goalsService from '../goals/goalsService';
 
-const initialState = {};
+const initialState = {
+  goals: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+};
+
+export const getGoals = createAsyncThunk('goals/getGoals', async (thunkAPI) => {
+  try {
+    return await goalsService.getGoals();
+  } catch (err) {
+    const message =
+      (err.response && err.response.data && err.response.data.message) ||
+      err.message ||
+      err.toString();
+
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
+export const createGoal = createAsyncThunk(
+  'goals/createGoal',
+  async (thunkAPI) => {
+    try {
+      return await goalsService.createGoal();
+    } catch (err) {
+      const message =
+        (err.response && err.response.data && err.response.data.message) ||
+        err.message ||
+        err.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const updateGoal = createAsyncThunk(
+  'goals/updateGoal',
+  async (thunkAPI) => {
+    try {
+      return await goalsService.updateGoal();
+    } catch (err) {
+      const message =
+        (err.response && err.response.data && err.response.data.message) ||
+        err.message ||
+        err.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const deleteGoal = createAsyncThunk(
+  'goals/deleteGoal',
+  async (thunkAPI) => {
+    try {
+      return await goalsService.deleteGoal();
+    } catch (err) {
+      const message =
+        (err.response && err.response.data && err.response.data.message) ||
+        err.message ||
+        err.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const goalsSlice = createSlice({
   name: 'goals',
   initialState,
-  reducers: {},
+  reducers: {
+    reset: (state) => initialState,
+  },
   extraReducers: (builder) => {},
 });
 
+export const { reset } = goalsSlice.actions;
 export default goalsSlice.reducer;
